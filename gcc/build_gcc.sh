5045536157ba80946682af155422f0b32e4ce5d7
“sunshine-study” <1274455999@163.com>
2024-06-05 14:09:28 +0800

    Updata gcc/build_gcc.sh

--- gcc/build_gcc.sh
+++ gcc/build_gcc.sh
@@ -2,7 +2,10 @@ cd /home/dushuai/binutils
 # 更新软件源
 # sudo apt update
 # 安装ctags
-sudo apt install universal-ctags libgmp-dev libmpfr-dev libmpc-dev libisl-dev zlib1g-dev
+sudo apt update
+sudo apt install universal-ctags 
+sudo apt install libgmp-dev libmpfr-dev libmpc-dev libisl-dev zlib1g-dev
+sudo apt-get install --reinstall libc6-dev
 # 下载gcc-13.3.0
 wget -P ~ 'https://mirrors.nju.edu.cn/gnu/gcc/gcc-13.3.0/gcc-13.3.0.tar.gz'
 # 将gcc压缩包移到我的binutils目录中
@@ -19,5 +22,9 @@ mkdir build install
 cd build/
 # 安装ubuntu32位开发库
 # sudo apt-get install gcc-multilib g++-multilib
-CFLAGS="-g -O0" CXXFLAGS="-g -O0" ../../configure --prefix=/home/dushuai/binutils/gcc-13.3.0/myfile/install/ --enable-multilib
+CFLAGS="-g -O0" CXXFLAGS="-g -O0" ../../configure --prefix=/home/dushuai/binutils/gcc-13.3.0/myfile/install/ --disable-multilib
 make -j$(nproc) && make install
+
+# 清理源码压缩包
+cd /home/dushuai/binutils
+rm -rf gcc-13.3.0.tar.gz
