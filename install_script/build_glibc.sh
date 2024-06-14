9dc94151a3b0af863923f0effb41be53ea5c1d9b
“sunshine-study” <1274455999@163.com>
2024-06-14 16:22:05 +0800

    Update install_script/build_glibc.sh

--- install_script/build_glibc.sh
+++ install_script/build_glibc.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
+set -e  # 出错时立即退出
+set -u  # 对未定义变量执行操作时退出
+
 # 更新软件源
 sudo apt update
 
-cd ~/binutils
+# 设置工作目录
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BINUTILS_DIR="$HOME/binutils"
+GLIBC_DIR="$BINUTILS_DIR/glibc-2.35"
 
+# 确保目录存在
+mkdir -p "$BINUTILS_DIR"
+
+# 切换到工作目录
+cd "$BINUTILS_DIR"
+
+# 清理旧文件
 rm -rf glibc-2.35*
 
-# 源码下载
+# 下载 glibc 源码
 wget http://ftp.gnu.org/pub/gnu/glibc/glibc-2.35.tar.gz
-# 解压
+
+# 解压并编译安装
 tar -zxf glibc-2.35.tar.gz
 cd glibc-2.35
-mkdir build && cd build
-../configure --prefix=/home/dushuai/tools/glibc-2.35 CFLAGS="-Og -g -g3 -ggdb -gdwarf-4" CXXFLAGS="-Og -g -g3 -ggdb -gdwarf-4" --disable-werror
+mkdir -p build && cd build
+../configure --prefix="$HOME/tools/glibc-2.35" \
+             CFLAGS="-Og -g -g3 -ggdb -gdwarf-4" \
+             CXXFLAGS="-Og -g -g3 -ggdb -gdwarf-4" \
+             --disable-werror \
+             --enable-debug \
+             --enable-debuginfod
+
 make -j4
 make install
 
-# 清理无用压缩包
-rm -rf glibc-2.35.tar.gz
+# 清理无用文件
+rm -rf "$BINUTILS_DIR/glibc-2.35.tar.gz"
 
+echo "glibc 2.35 安装完成。"
