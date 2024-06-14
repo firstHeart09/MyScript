8713b59ec493a3e38ddf3a099d9b807746478e6b
“sunshine-study” <1274455999@163.com>
2024-06-14 15:01:32 +0800

    Add build_glibc.sh

--- install_script/build_glibc.sh
+++ install_script/build_glibc.sh
@@ -4,15 +4,18 @@ sudo apt update
 
 cd ~/binutils
 
-rm -rf glibc-2.39*
+rm -rf glibc-2.35*
 
 # 源码下载
-wget http://ftp.gnu.org/pub/gnu/glibc/glibc-2.39.tar.gz
+wget http://ftp.gnu.org/pub/gnu/glibc/glibc-2.35.tar.gz
 # 解压
-tar -zxf glibc-2.39.tar.gz
-cd glibc-2.39
+tar -zxf glibc-2.35.tar.gz
+cd glibc-2.35
 mkdir build && cd build
-../configure --prefix=/home/dushuai/tools/glibc-2.39 CFLAGS="-Og -g -g3 -ggdb -gdwarf-4" CXXFLAGS="-Og -g -g3 -ggdb -gdwarf-4" --disable-werror
+../configure --prefix=/home/dushuai/tools/glibc-2.35 CFLAGS="-Og -g -g3 -ggdb -gdwarf-4" CXXFLAGS="-Og -g -g3 -ggdb -gdwarf-4" --disable-werror
 make -j4
 make install
+
+# 清理无用压缩包
+rm -rf glibc-2.35.tar.gz
 
