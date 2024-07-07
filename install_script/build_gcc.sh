d75c4f888b452fe8a2e277268574b9a64d8a9751
“sunshine-study” <1274455999@163.com>
2024-07-07 10:56:16 +0800

    update

--- install_script/build_gcc.sh
+++ install_script/build_gcc.sh
@@ -1,5 +1,5 @@
 cd /home/dushuai/binutils
-rm -rf gcc-13.3.0
+rm -rf gcc-14.1.0
 # 更新软件源
 # sudo apt update
 # 安装ctags
@@ -8,22 +8,22 @@ sudo apt install universal-ctags
 sudo apt install libgmp-dev libmpfr-dev libmpc-dev libisl-dev zlib1g-dev
 sudo apt-get install --reinstall libc6-dev
 # 下载gcc-13.3.0
-wget -P ~ 'https://mirrors.nju.edu.cn/gnu/gcc/gcc-13.3.0/gcc-13.3.0.tar.gz'
+wget -P ~ 'https://mirrors.nju.edu.cn/gnu/gcc/gcc-14.1.0/gcc-14.1.0.tar.gz'
 # 将gcc压缩包移到我的binutils目录中
-mv ../gcc-13.3.0.tar.gz ./
+mv ../gcc-14.1.0.tar.gz ./
 # 对压缩包进行解压
-tar xzvf gcc-13.3.0.tar.gz
+tar xzvf gcc-14.1.0.tar.gz
 # 进入gcc-13.3.0目录
-cd gcc-13.3.0/
+cd gcc-14.1.0/
 # 生成ctags标签
 ctags -R
 # 创建build目录
 mkdir build && cd cbuild/
 # 安装ubuntu32位开发库
 # sudo apt-get install gcc-multilib g++-multilib
-CFLAGS="-g -O0" CXXFLAGS="-g -O0" /home/dushuai/binutils/gcc-13.3.0/configure --prefix=/home/dushuai/tools/gcc-13.3.0 --disable-multilib
+CFLAGS="-g -O0" CXXFLAGS="-g -O0" /home/dushuai/binutils/gcc-14.1.0/configure --prefix=/home/dushuai/tools/gcc-14.1.0 --disable-multilib
 make -j$(nproc) && make install
 
 # 清理源码压缩包
 cd /home/dushuai/binutils
-rm -rf gcc-13.3.0.tar.gz
+rm -rf gcc-14.1.0.tar.gz
