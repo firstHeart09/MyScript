ec9ebcacca1f5617333660e355967d2507e665d7
firstHeart09 <1274455304@qq.com>
2024-07-07 20:51:11 +0800

    Update the dir install_script

--- install_script/build_gcc.sh
+++ install_script/build_gcc.sh
@@ -1,29 +1,48 @@
-cd /home/dushuai/binutils
-rm -rf gcc-14.1.0
-# 更新软件源
-# sudo apt update
-# 安装ctags
+#!/bin/bash
+set -e  # 出错时立即退出
+set -u  # 对未定义变量执行操作时退出
+
+# 更新软件源并安装必要依赖
 sudo apt update
-sudo apt install universal-ctags 
+sudo apt install universal-ctags
 sudo apt install libgmp-dev libmpfr-dev libmpc-dev libisl-dev zlib1g-dev
 sudo apt-get install --reinstall libc6-dev
-# 下载gcc-13.3.0
-wget -P ~ 'https://mirrors.nju.edu.cn/gnu/gcc/gcc-14.1.0/gcc-14.1.0.tar.gz'
-# 将gcc压缩包移到我的binutils目录中
-mv ../gcc-14.1.0.tar.gz ./
-# 对压缩包进行解压
-tar xzvf gcc-14.1.0.tar.gz
-# 进入gcc-13.3.0目录
-cd gcc-14.1.0/
-# 生成ctags标签
+
+# 设置工作目录和版本号
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BINUTILS_DIR="$HOME/source"
+GCC_VERSION="14.1.0"
+GCC_TAR="gcc-${GCC_VERSION}.tar.gz"
+GCC_URL="https://mirrors.nju.edu.cn/gnu/gcc/gcc-${GCC_VERSION}/${GCC_TAR}"
+
+# 确保目录存在
+mkdir -p "$BINUTILS_DIR"
+
+# 下载 gcc 源代码
+if [ ! -f "$GCC_TAR" ]; then
+    wget -P "$BINUTILS_DIR" "$GCC_URL"
+fi
+
+# 切换到工作目录
+cd "$BINUTILS_DIR"
+
+# 解压缩源代码
+tar -xzvf "$GCC_TAR"
+cd "gcc-${GCC_VERSION}"
+
+# 生成 ctags 标签
 ctags -R
-# 创建build目录
-mkdir build && cd cbuild/
-# 安装ubuntu32位开发库
-# sudo apt-get install gcc-multilib g++-multilib
-CFLAGS="-g -O0" CXXFLAGS="-g -O0" /home/dushuai/binutils/gcc-14.1.0/configure --prefix=/home/dushuai/tools/gcc-14.1.0 --disable-multilib
+
+# 创建 build 目录并进入
+mkdir build && cd build
+
+# 配置、编译和安装 gcc
+CFLAGS="-g -O0" CXXFLAGS="-g -O0" ../configure --prefix="$HOME/tools/gcc-${GCC_VERSION}" --disable-multilib
 make -j$(nproc) && make install
 
-# 清理源码压缩包
-cd /home/dushuai/binutils
-rm -rf gcc-14.1.0.tar.gz
+# 返回 binutils 目录并清理工作目录
+cd "$BINUTILS_DIR"
+rm -rf "$GCC_TAR" gcc-${GCC_VERSION}
+
+echo "gcc ${GCC_VERSION} 安装完成。"
+
--- install_script/build_glibc.sh
+++ install_script/build_glibc.sh
@@ -4,10 +4,10 @@ set -u  # 对未定义变量执行操作时退出
 
 # 更新软件源
 sudo apt update
-
+sudo apt install gawk bison
 # 设置工作目录
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-BINUTILS_DIR="$HOME/binutils"
+BINUTILS_DIR="$HOME/source"
 GLIBC_DIR="$BINUTILS_DIR/glibc-2.35"
 
 # 确保目录存在
@@ -17,16 +17,16 @@ mkdir -p "$BINUTILS_DIR"
 cd "$BINUTILS_DIR"
 
 # 清理旧文件
-rm -rf glibc-2.23*
+rm -rf glibc-2.35*
 
 # 下载 glibc 源码
-wget http://ftp.gnu.org/pub/gnu/glibc/glibc-2.23.tar.gz
+wget http://ftp.gnu.org/pub/gnu/glibc/glibc-2.35.tar.gz
 
 # 解压并编译安装
-tar -zxf glibc-2.23.tar.gz
-cd glibc-2.23
+tar -zxf glibc-2.35.tar.gz
+cd glibc-2.35
 mkdir -p build && cd build
-../configure --prefix="$HOME/tools/glibc-2.23" \
+../configure --prefix="$HOME/tools/glibc-2.35" \
              CFLAGS="-Og -g -g3 -ggdb -gdwarf-4" \
              CXXFLAGS="-Og -g -g3 -ggdb -gdwarf-4" \
              --disable-werror \
@@ -37,6 +37,6 @@ make -j4
 make install
 
 # 清理无用文件
-rm -rf "$BINUTILS_DIR/glibc-2.23.tar.gz"
+rm -rf "$BINUTILS_DIR/glibc-2.35.tar.gz"
 
-echo "glibc 2.23 安装完成。"
+echo "glibc 2.35 安装完成。"
