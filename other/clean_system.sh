90bf46fbedec66a7dd5b698809251d7b107a2e01
firstHeart09 <1274455304@qq.com>
2024-07-08 10:58:11 +0800

    add other/vim_config.sh

--- other/clean_system.sh
+++ other/clean_system.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # 自动清理不再需要的软件包
-sudo apt autoremove
+apt autoremove
 # 清理APT缓存
-sudo apt autoclean
+apt autoclean
 # 清理已删除软件包的配置文件
-sudo apt-get purge
+apt-get purge
 # 清理临时文件
-sudo rm -rf /tmp/*
+rm -rf /tmp/*
 # 清理systemd日志记录
-sudo journalctl --vacuum-time=3d
+journalctl --vacuum-time=3d
 # 移除旧版本的Snap应用程序
 set -eu
 snap list --all | awk '/disabled/{print $1, $3}' |
