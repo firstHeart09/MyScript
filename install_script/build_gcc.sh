2521a7b949a01e29a95bd512ca732c7505cd44f9
firstHeart09 <1274455304@qq.com>
2024-09-22 14:33:57 +0800

    Updata install_script/build_gcc.sh

--- install_script/build_gcc.sh
+++ install_script/build_gcc.sh
@@ -3,9 +3,8 @@ set -e  # 出错时立即退出
 set -u  # 对未定义变量执行操作时退出
 
 # 更新软件源并安装必要依赖
-sudo apt update
-sudo apt install universal-ctags
-sudo apt install libgmp-dev libmpfr-dev libmpc-dev libisl-dev zlib1g-dev
+sudo apt-get update
+sudo apt-get install universal-ctags libgmp-dev libmpfr-dev libmpc-dev libisl-dev zlib1g-dev
 sudo apt-get install --reinstall libc6-dev
 
 # 设置工作目录和版本号
@@ -18,16 +17,20 @@ GCC_URL="https://mirrors.nju.edu.cn/gnu/gcc/gcc-${GCC_VERSION}/${GCC_TAR}"
 # 确保目录存在
 mkdir -p "$BINUTILS_DIR"
 
-# 下载 gcc 源代码
-if [ ! -f "$GCC_TAR" ]; then
+# 下载 gcc 源代码并检查下载结果
+if [ ! -f "$BINUTILS_DIR/$GCC_TAR" ]; then
     wget -P "$BINUTILS_DIR" "$GCC_URL"
+    if [ $? -ne 0 ]; then
+        echo "下载失败: $GCC_URL"
+        exit 1
+    fi
 fi
 
 # 切换到工作目录
 cd "$BINUTILS_DIR"
 
 # 解压缩源代码
-tar -xzvf "$GCC_TAR"
+tar -xzvf "$BINUTILS_DIR/$GCC_TAR"
 cd "gcc-${GCC_VERSION}"
 
 # 生成 ctags 标签
@@ -40,9 +43,8 @@ mkdir build && cd build
 CFLAGS="-g -O0" CXXFLAGS="-g -O0" ../configure --prefix="$HOME/tools/gcc-${GCC_VERSION}" --disable-multilib
 make -j$(nproc) && make install
 
-# 返回 binutils 目录并清理工作目录
-cd "$BINUTILS_DIR"
-rm -rf "$GCC_TAR"
+# 清理下载的文件和源码目录
+rm -rf "$BINUTILS_DIR/$GCC_TAR"
 
 echo "gcc ${GCC_VERSION} 安装完成。"
 
